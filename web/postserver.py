9d4251869997ab6e750b09ac6e77c126f5006305
Robin Wei <www.jianwei@outlook.com>
2025-07-16 16:27:47 +0800

    Update postserver.py

--- web/postserver.py
+++ web/postserver.py
@@ -1,4 +1,3 @@
-                                                                                                                                                                   
 from flask import Flask, request, jsonify
 import json
 
@@ -15,4 +14,3 @@ def coins():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
